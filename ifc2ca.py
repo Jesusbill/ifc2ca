091210a20afca06d717566bd005d9ea798a34839
Jesusbill <ipc@aethereng.com>
2020-03-10 13:26:07 +0100

    intermediate state add structural-surface-member

--- ifc2ca.py
+++ ifc2ca.py
@@ -20,6 +20,10 @@ class IFC2CA:
                 'connections': self.get_connections()
             }
 
+            print('Number of elements: ', len(self.result['elements']))
+            print('Number of supports: ', len(self.result['supports']))
+            print('Number of connections: ', len(self.result['connections']))
+
     def get_units(self):
         # TODO: unhardcode
         units = {}
@@ -42,32 +46,53 @@ class IFC2CA:
         return elements
 
     def get_element_data(self, element):
-        representation = self.get_representation(element)
-        material_profile = self.get_material_profile(element)
-        if not representation or not material_profile:
-            print(representation, material_profile)
-            return
-        for connection in element.ConnectedBy:
-            if connection.RelatedStructuralConnection:
-                self.supports.append(connection.RelatedStructuralConnection)
-        return {
-            'ifcName': element.is_a() + '|' + str(element.id()),
-            'name': element.Name,
-            'id': element.GlobalId,
-            'geometryType': self.get_geometry_type(representation),
-            'geometry': self.get_geometry(representation),
-            'rotation': 0, # TODO: unhardcode
-            'material': self.get_material_properties(material_profile),
-            'profile': self.get_profile_properties(material_profile),
-            'elementType': 'EulerBeam' # TODO: unhardcode
-        }
+        if element.is_a('IfcStructuralCurveMember'):
+            representation = self.get_representation(element, 'Edge')
+            material_profile = self.get_material_profile(element)
+            if not representation or not material_profile:
+                print(representation, material_profile)
+                return
+            for connection in element.ConnectedBy:
+                if connection.RelatedStructuralConnection:
+                    self.supports.append(connection.RelatedStructuralConnection)
+            return {
+                'ifcName': element.is_a() + '|' + str(element.id()),
+                'name': element.Name,
+                'id': element.GlobalId,
+                'geometryType': self.get_geometry_type(representation),
+                'geometry': self.get_geometry(representation),
+                'rotation': 0, # TODO: unhardcode
+                'material': self.get_material_properties(material_profile.Material),
+                'profile': self.get_profile_properties(material_profile.Profile),
+                'elementType': 'EulerBeam' # TODO: unhardcode
+            }
 
-    def get_supports(self, numberOfConnectedMembers=1):
+        elif element.is_a('IfcStructuralSurfaceMember'):
+            representation = self.get_representation(element, 'Face')
+            material = self.get_material_profile(element)
+            if not representation:
+                print(representation)
+                return
+            for connection in element.ConnectedBy:
+                if connection.RelatedStructuralConnection:
+                    self.supports.append(connection.RelatedStructuralConnection)
+            return {
+                'ifcName': element.is_a() + '|' + str(element.id()),
+                'name': element.Name,
+                'id': element.GlobalId,
+                'thickness': element.Thickness,
+                # 'geometryType': self.get_geometry_type(representation),
+                'geometry': self.get_geometry(representation),
+                'material': self.get_material_properties(material),
+                # 'elementType': 'EulerBeam' # TODO: unhardcode
+            }
+
+    def get_supports(self):
         supports = []
         for support in self.supports:
             if support.GlobalId in [s['id'] for s in supports]:
                 continue
-            if len(support.ConnectsStructuralMembers) != numberOfConnectedMembers:
+            if len(support.ConnectsStructuralMembers) != 1:
                 continue
             supports.append({
                 'ifcName': support.is_a() + '|' + str(support.id()),
@@ -80,8 +105,23 @@ class IFC2CA:
             })
         return supports
 
-    def get_connections(self, numberOfConnectedMembers=2):
-        return self.get_supports(numberOfConnectedMembers)
+    def get_connections(self):
+        supports = []
+        for support in self.supports:
+            if support.GlobalId in [s['id'] for s in supports]:
+                continue
+            if len(support.ConnectsStructuralMembers) <= 1:
+                continue
+            supports.append({
+                'ifcName': support.is_a() + '|' + str(support.id()),
+                'name': support.Name,
+                'id': support.GlobalId,
+                'geometryType': self.get_support_geometry_type(support),
+                'geometry': self.get_support_geometry(support),
+                'appliedCondition': self.get_support_input(support),
+                'relatedElements': self.get_support_members(support)
+            })
+        return supports
 
     def get_support_geometry_type(self, support):
         if support.is_a('IfcStructuralPointConnection'):
@@ -89,8 +129,8 @@ class IFC2CA:
 
     def get_support_geometry(self, support):
         # TODO: make more robust
-        if support.ObjectPlacement:
-            return support.ObjectPlacement.RelativePlacement.Location.Coordinates
+        # if support.ObjectPlacement:
+        #     return support.ObjectPlacement.RelativePlacement.Location.Coordinates
         representation = self.get_support_representation(support)
         item = representation.Items[0]
         if item.is_a('IfcVertexPoint'):
@@ -109,7 +149,24 @@ class IFC2CA:
         return None
 
     def get_support_members(self, support):
-        return [s.RelatingStructuralMember.is_a() + '|' + str(s.RelatingStructuralMember.id()) for s in support.ConnectsStructuralMembers]
+        answer = []
+        for s in support.ConnectsStructuralMembers:
+            if s.is_a('IfcRelConnectsStructuralMember'):
+                answer.append({
+                    'ifcName': s.is_a() + '|' + str(s.id()),
+                    'element': s.RelatingStructuralMember.is_a() + '|' + str(s.RelatingStructuralMember.id()),
+                    'geometryPointIndex': None
+                })
+
+            if s.is_a('IfcRelConnectsWithEccentricity'):
+                answer.append({
+                    'ifcName': s.is_a() + '|' + str(s.id()),
+                    'element': s.RelatingStructuralMember.is_a() + '|' + str(s.RelatingStructuralMember.id()),
+                    'geometryPointIndex': 1 if s.ConnectionConstraint.EccentricityInX < 0 else 2,
+                    'linkLength': abs(s.ConnectionConstraint.EccentricityInX)
+                })
+
+        return answer
 
     def get_support_representation(self, element):
         if not element.Representation:
@@ -118,14 +175,26 @@ class IFC2CA:
             rep = self.get_specific_representation(representation, 'Reference', 'Vertex')
             if rep:
                 return rep
+        else:
+            # print('Trying without rep identifier')
+            for representation in element.Representation.Representations:
+                rep = self.get_specific_representation(representation, None, 'Vertex')
+                if rep:
+                    return rep
 
-    def get_representation(self, element):
+    def get_representation(self, element, rep_type):
         if not element.Representation:
             return None
         for representation in element.Representation.Representations:
-            rep = self.get_specific_representation(representation, 'Reference', 'Edge')
+            rep = self.get_specific_representation(representation, 'Reference', rep_type)
             if rep:
                 return rep
+        else:
+            # print('Trying without rep identifier')
+            for representation in element.Representation.Representations:
+                rep = self.get_specific_representation(representation, None, rep_type)
+                if rep:
+                    return rep
 
     def get_specific_representation(self, representation, rep_id, rep_type):
         if representation.RepresentationIdentifier == rep_id \
@@ -152,6 +221,13 @@ class IFC2CA:
                 self.get_coordinate(item.EdgeEnd.VertexGeometry)
             ]
 
+        if item.is_a('IfcFaceSurface'):
+            edges = item.Bounds[0].Bound.EdgeList
+            coords = []
+            for edge in edges:
+                coords.append(self.get_coordinate(edge.EdgeElement.EdgeStart.VertexGeometry))
+            return coords
+
     def get_coordinate(self, point):
         if point.is_a('IfcCartesianPoint'):
             return point.Coordinates
@@ -168,25 +244,38 @@ class IFC2CA:
                 return material.MaterialProfiles[0]
             if material.is_a('IfcMaterialProfileSetUsage'):
                 return material.ForProfileSet.MaterialProfiles[0]
+            if material.is_a('IfcMaterial'):
+                return material
 
-    def get_material_properties(self, profile):
-        psets = profile.Material.HasProperties
+    def get_material_properties(self, material):
+        psets = material.HasProperties
+
+        if self.get_pset_properties(psets, 'Pset_MaterialMechanical'):
+            mechProps = self.get_pset_properties(psets, 'Pset_MaterialMechanical')
+        else:
+            mechProps = self.get_pset_properties(psets, None)
+
+        if self.get_pset_properties(psets, 'Pset_MaterialCommon'):
+            commonProps = self.get_pset_properties(psets, 'Pset_MaterialCommon')
+        else:
+            commonProps = self.get_pset_properties(psets, None)
+
         return {
-            'ifcName': profile.Material.is_a() + '|' + str(profile.Material.id()),
-            'mechProps': self.get_pset_properties(psets, 'Pset_MaterialMechanical'),
-            'commonProps': self.get_pset_properties(psets, 'Pset_MaterialCommon')
+            'ifcName': material.is_a() + '|' + str(material.id()),
+            'mechProps': mechProps,
+            'commonProps':commonProps
         }
 
     def get_pset_property(self, psets, pset_name, prop_name):
         for pset in psets:
-            if pset.Name == pset_name:
+            if pset.Name == pset_name or pset_name is None:
                 for prop in pset.Properties:
                     if prop.Name == prop_name:
                         return prop.NominalValue.wrappedValue
 
     def get_pset_properties(self, psets, pset_name):
         for pset in psets:
-            if pset.Name == pset_name:
+            if pset.Name == pset_name or pset_name is None:
                 d = {}
                 for prop in pset.Properties:
                     propName = prop.Name[0].lower() + prop.Name[1:]
@@ -194,34 +283,36 @@ class IFC2CA:
                 return d
 
     def get_profile_properties(self, profile):
-        if profile.Profile.is_a('IfcRectangleProfileDef'):
+        if profile.is_a('IfcRectangleProfileDef'):
             return {
-                'ifcName': profile.Profile.is_a() + '|' + str(profile.Profile.id()),
+                'ifcName': profile.is_a() + '|' + str(profile.id()),
                 'profileType': 'rectangular',
                 'profileVariation': 'constant',
-                'xDim': profile.Profile.XDim,
-                'yDim': profile.Profile.YDim
+                'xDim': profile.XDim,
+                'yDim': profile.YDim
             }
 
-        if profile.Profile.is_a('IfcIShapeProfileDef'):
-            psets = profile.Profile.HasProperties
+        if profile.is_a('IfcIShapeProfileDef'):
+            psets = profile.HasProperties
             return {
-                'ifcName': profile.Profile.is_a() + '|' + str(profile.Profile.id()),
+                'ifcName': profile.is_a() + '|' + str(profile.id()),
                 'profileType': 'iSymmetrical',
                 'mechProps': self.get_pset_properties(psets, 'Pset_ProfileMechanical'),
                 'commonProps': {
-                    'flangeThickness': profile.Profile.FlangeThickness,
-                    'webThickness': profile.Profile.WebThickness,
-                    'overallDepth': profile.Profile.OverallDepth,
-                    'overallWidth': profile.Profile.OverallWidth,
-                    'filletRadius': profile.Profile.FilletRadius,
-                    'profileName': profile.Profile.ProfileName,
-                    'profileType': profile.Profile.ProfileType
+                    'flangeThickness': profile.FlangeThickness,
+                    'webThickness': profile.WebThickness,
+                    'overallDepth': profile.OverallDepth,
+                    'overallWidth': profile.OverallWidth,
+                    'filletRadius': profile.FilletRadius,
+                    'profileName': profile.ProfileName,
+                    'profileType': profile.ProfileType
                 }
             }
 
-ifc2ca = IFC2CA('ifcModels/portal_01.ifc')
+fileName = 'building_01'
+ifc2ca = IFC2CA('ifcModels/' + fileName + '.ifc')
 ifc2ca.convert()
 # print(json.dumps(ifc2ca.result, indent=4))
-with open('ifc2ca.json', 'w') as f:
+# with open('ifc2ca.json', 'w') as f:
+with open(('ifcModels/' + fileName + '.json') , 'w') as f:
     f.write(json.dumps(ifc2ca.result, indent = 4))
--- scriptSalome.py
+++ scriptSalome.py
@@ -23,6 +23,7 @@ units = data['units']
 elements = data['elements']
 mesh = data['mesh']
 supports = data['supports']
+connections = data['connections']
 
 meshSize = mesh['meshSize']
 
@@ -54,6 +55,21 @@ def makePoint(pl):
     (x, y, z) = pl
     return geompy.MakeVertex(x, y, z)
 
+def makeFace(pl):
+    '''Function to define a Face from
+        a polyline (list of points)'''
+
+    pointList = [None for _ in range(len(pl))]
+    for ip, (x, y, z) in enumerate(pl):
+        pointList[ip] = geompy.MakeVertex(x, y, z)
+
+    LineList = [None for _ in range(len(pl))]
+    for ip, P2 in enumerate(pointList):
+        P1 = pointList[ip - 1]
+        LineList[ip] = geompy.MakeLineTwoPnt(P1, P2)
+
+    return geompy.MakeFaceWires(LineList, 1)
+
 def getGroupName(name):
     if len(name) <= 24:
         return name
@@ -83,11 +99,11 @@ geompy.addToStudy( OX, 'OX' )
 geompy.addToStudy( OY, 'OY' )
 geompy.addToStudy( OZ, 'OZ' )
 
-buildElements_1D = []
-buildElements_1D.extend(elements)
-
+buildElements_1D = [e for e in elements if len(e['geometry']) == 2]
+buildElements_2D = [e for e in elements if len(e['geometry']) > 2]
 buildElements = []
 buildElements.extend(buildElements_1D)
+buildElements.extend(buildElements_2D)
 
 ### Define entities ###
 start_time = time.time()
@@ -95,31 +111,76 @@ print ('Defining Object Geometry')
 init_time = start_time
 
 lineObjs = [None for _ in range(len(buildElements_1D))]
-# For each straightLine object
-for i,line in enumerate(buildElements_1D):
-    # Make line
-    lineObjs[i] = makeLine(line['geometry'])
-    geompy.addToStudy(lineObjs[i], getGroupName(str(line['ifcName'])))
+if buildElements_1D:
+    # For each straightLine object
+    for i,line in enumerate(buildElements_1D):
+        # Make line
+        lineObjs[i] = makeLine(line['geometry'])
+        geompy.addToStudy(lineObjs[i], getGroupName(str(line['ifcName'])))
 
-lineSetObj = geompy.MakePartition(lineObjs, [], [], [], geompy.ShapeType["EDGE"], 0, [], 1)
-geompy.addToStudy(lineSetObj, 'lines')
+    lineSetObj = geompy.MakePartition(lineObjs, [], [], [], geompy.ShapeType["EDGE"], 0, [], 1)
+    geompy.addToStudy(lineSetObj, 'lines')
+
+faceObjs = [None for _ in range(len(buildElements_2D))]
+if buildElements_2D:
+    # For each straightLine object
+    for i,face in enumerate(buildElements_2D):
+        # Make face
+        faceObjs[i] = makeFace(face['geometry'])
+        geompy.addToStudy(faceObjs[i], getGroupName(str(face['ifcName'])))
+
+    faceSetObj = geompy.MakePartition(faceObjs, [], [], [], geompy.ShapeType["FACE"], 0, [], 1)
+    geompy.addToStudy(faceSetObj, 'faces')
 
 pointObjs = [None for _ in range(len(supports))]
 # For each straightLine object
-for i,point in enumerate(supports):
-    # Make point
-    pointObjs[i] = makePoint(point['geometry'])
-    geompy.addToStudy(pointObjs[i], getGroupName(str(point['ifcName'])))
+if supports:
+    for i,point in enumerate(supports):
+        # Make point
+        pointObjs[i] = makePoint(point['geometry'])
+        geompy.addToStudy(pointObjs[i], getGroupName(str(point['ifcName'])))
 
-# pointSetObj = geompy.MakePartition(pointObjs, [], [], [], geompy.ShapeType["EDGE"], 0, [], 1)
-# geompy.addToStudy(pointSetObj, 'points')
+    # pointSetObj = geompy.MakePartition(pointObjs, [], [], [], geompy.ShapeType["EDGE"], 0, [], 1)
+    # geompy.addToStudy(pointSetObj, 'points')
+
+connectionObjs = [None for _ in range(len(connections))]
+# For each straightLine object
+if connections:
+    for i,connection in enumerate(connections):
+        # Make connection
+        connPoint = makePoint(connection['geometry'])
+        connLines = []
+        for e in connection['relatedElements']:
+            if e['geometryPointIndex'] is not None:
+                p = [elem for elem in elements if elem['ifcName'] == e['element']][0]['geometry'][e['geometryPointIndex'] - 1]
+                (x, y, z) = p
+                P = geompy.MakeVertex(x, y, z)
+                connLines.append(
+                    geompy.MakeLineTwoPnt(connPoint, P)
+                )
+
+        connection['rigidLinks'] = len(connLines) > 0
+        if connection['rigidLinks']:
+            connectionObjs[i] = geompy.MakePartition([connPoint] + connLines, [], [], [], geompy.ShapeType["EDGE"], 0, [], 1)
+        else:
+            connectionObjs[i] = connPoint
+
+        geompy.addToStudy(connectionObjs[i], getGroupName(str(connection['ifcName'])))
+
+    # pointSetObj = geompy.MakePartition(pointObjs, [], [], [], geompy.ShapeType["EDGE"], 0, [], 1)
+    # geompy.addToStudy(pointSetObj, 'points')
 
 # Make assemble of Building Object
 bldObjs = []
 bldObjs.extend(lineObjs)
+bldObjs.extend(faceObjs)
 bldObjs.extend(pointObjs)
+bldObjs.extend(connectionObjs)
 
-bldComp = geompy.MakePartition(bldObjs, [], [], [], geompy.ShapeType["EDGE"], 0, [], 1)
+if buildElements_2D:
+    bldComp = geompy.MakePartition(bldObjs, [], [], [], geompy.ShapeType["FACE"], 0, [], 1)
+else:
+    bldComp = geompy.MakePartition(bldObjs, [], [], [], geompy.ShapeType["EDGE"], 0, [], 1)
 geompy.addToStudy(bldComp, 'bldComp')
 
 elapsed_time = time.time() - init_time
@@ -131,13 +192,28 @@ for i,line in enumerate(buildElements_1D):
     lineObjs[i] = geompy.GetInPlace(bldComp, lineObjs[i])
     geompy.addToStudyInFather(bldComp, lineObjs[i], getGroupName(str(line['ifcName'])))
 
+for i,face in enumerate(buildElements_2D):
+    # Make face
+    faceObjs[i] = geompy.GetInPlace(bldComp, faceObjs[i])
+    geompy.addToStudyInFather(bldComp, faceObjs[i], getGroupName(str(face['ifcName'])))
+
 for i,point in enumerate(supports):
     # Make point
     pointObjs[i] = geompy.GetInPlace(bldComp, pointObjs[i])
     geompy.addToStudyInFather(bldComp, pointObjs[i], getGroupName(str(point['ifcName'])))
 
-lineSetObj = geompy.GetInPlace(bldComp, lineSetObj)
-geompy.addToStudyInFather(bldComp, lineSetObj, 'lines')
+for i,connection in enumerate(connections):
+    # Make connection
+    connectionObjs[i] = geompy.GetInPlace(bldComp, connectionObjs[i])
+    geompy.addToStudyInFather(bldComp, connectionObjs[i], getGroupName(str(connection['ifcName'])))
+
+if buildElements_1D:
+    lineSetObj = geompy.GetInPlace(bldComp, lineSetObj)
+    geompy.addToStudyInFather(bldComp, lineSetObj, 'lines')
+
+if buildElements_2D:
+    faceSetObj = geompy.GetInPlace(bldComp, faceSetObj)
+    geompy.addToStudyInFather(bldComp, faceSetObj, 'faces')
 
 # pointSetObj = geompy.GetInPlace(bldComp, pointSetObj)
 # geompy.addToStudyInFather(bldComp, pointSetObj, 'points')
@@ -161,13 +237,24 @@ Mesh_1 = smesh.Mesh(bldComp)
 Regular_1D = Mesh_1.Segment()
 Local_Length_1 = Regular_1D.LocalLength(meshSize, None, tolLoc)
 
+NETGEN_2D_ONLY = Mesh_1.Triangle(algo=smeshBuilder.NETGEN_2D)
+NETGEN_2D_Pars = NETGEN_2D_ONLY.Parameters()
+NETGEN_2D_Pars.SetMaxSize(meshSize)
+NETGEN_2D_Pars.SetOptimize(1)
+NETGEN_2D_Pars.SetFineness(2)
+NETGEN_2D_Pars.SetMinSize(meshSize/5)
+NETGEN_2D_Pars.SetUseSurfaceCurvature(1)
+NETGEN_2D_Pars.SetQuadAllowed(1)
+NETGEN_2D_Pars.SetSecondOrder(0)
+NETGEN_2D_Pars.SetFuseEdges(254)
+
 isDone = Mesh_1.Compute()
 
 ## Set names of Mesh objects
 smesh.SetName(Regular_1D.GetAlgorithm(), 'Regular_1D')
 smesh.SetName(Local_Length_1, 'Local_Length_1')
-# smesh.SetName(NETGEN_2D_ONLY.GetAlgorithm(), 'NETGEN_2D_ONLY')
-# smesh.SetName(NETGEN_2D_Pars, 'NETGEN_2D_Pars')
+smesh.SetName(NETGEN_2D_ONLY.GetAlgorithm(), 'NETGEN_2D_ONLY')
+smesh.SetName(NETGEN_2D_Pars, 'NETGEN_2D_Pars')
 # smesh.SetName(Quadrangle_2D.GetAlgorithm(), 'Quadrangle_2D')
 # smesh.SetName(Quadrangle_Pars, 'Quadrangle Parameters_1')
 smesh.SetName(Mesh_1.GetMesh(), 'bldMesh')
@@ -182,12 +269,28 @@ for i,line in enumerate(buildElements_1D):
     tempgroup = Mesh_1.GroupOnGeom(lineObjs[i], getGroupName(str(line['ifcName'])), SMESH.EDGE)
     smesh.SetName(tempgroup, getGroupName(str(line['ifcName'])))
 
+for i,face in enumerate(buildElements_2D):
+    tempgroup = Mesh_1.GroupOnGeom(faceObjs[i], getGroupName(str(face['ifcName'])), SMESH.FACE)
+    smesh.SetName(tempgroup, getGroupName(str(face['ifcName'])))
+
 for i,point in enumerate(supports):
     tempgroup = Mesh_1.GroupOnGeom(pointObjs[i], getGroupName(str(point['ifcName'])), SMESH.NODE)
     smesh.SetName(tempgroup, getGroupName(str(point['ifcName'])))
 
-tempgroup = Mesh_1.GroupOnGeom(lineSetObj, 'lines', SMESH.EDGE)
-smesh.SetName(tempgroup, 'lines')
+for i,connection in enumerate(connections):
+    tempgroup = Mesh_1.GroupOnGeom(connectionObjs[i], getGroupName(str(connection['ifcName'])), SMESH.NODE)
+    smesh.SetName(tempgroup, getGroupName(str(connection['ifcName'])))
+    if connection['rigidLinks']:
+        tempgroup = Mesh_1.GroupOnGeom(connectionObjs[i], getGroupName(str(connection['ifcName'])), SMESH.EDGE)
+        smesh.SetName(tempgroup, getGroupName(str(connection['ifcName'])))
+
+if buildElements_1D:
+    tempgroup = Mesh_1.GroupOnGeom(lineSetObj, 'lines', SMESH.EDGE)
+    smesh.SetName(tempgroup, 'lines')
+
+if buildElements_2D:
+    tempgroup = Mesh_1.GroupOnGeom(faceSetObj, 'faces', SMESH.FACE)
+    smesh.SetName(tempgroup, 'faces')
 
 elapsed_time = time.time() - init_time
 init_time += elapsed_time
